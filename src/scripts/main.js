d3d670845604fc82101f28a304dce52e187fb0f8
lehinfo-felix <lehinfo.felix@gmail.com>
2023-10-30 17:30:55 -0300

    feat: adicionando histórico

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,62 +1,61 @@
-// const buttonValues = ['/','%','7','8','9','*','4','5','6','-','1','2','3','+','00','0','.']
+const currentInput = document.querySelector(".currentInput");
+const answerScreen = document.querySelector(".answerScreen");
+const eraseButton = document.querySelector("#erase");
+const clearButton = document.querySelector("#clear");
+const evaluate = document.querySelector("#evaluate");
 
-// const renderButtons = (buttonValues) => {
-//     buttonValues.forEach(buttonValue => {
-//         const button = document.createElement('button')
-//         button.value = buttonValue
-//         button.className = 'fun_btn'
-//         button.type = 'button'
-//         button.innerHTML = buttonValue
-//         document.querySelector('.keypad-btns').appendChild(button)
-//     });
-// }
-// renderButtons(buttonValues)
+const buttons = document.querySelectorAll("button");
+const history = [];
 
-const currentInput = document.querySelector('.currentInput')
-const answerScreen = document.querySelector('.answerScreen')
-const eraseButton = document.querySelector('#erase')
-const clearButton = document.querySelector('#clear')
-const evaluate = document.querySelector('#evaluate')
-
-const buttons = document.querySelectorAll('button')
-
-let realTimeScreenValue = []
+let realTimeScreenValue = "";
 
 clearButton.addEventListener("click", () => {
-    realTimeScreenValue = ['']
-    answerScreen.innerHTML = 0
-    currentInput.className = 'currentInput'
-    answerScreen.className = 'answerScreen'
-    answerScreen.style.color = " rgba(150, 150, 150, 0.87)"
-})
+  realTimeScreenValue = "";
+  answerScreen.innerHTML = 0;
+  currentInput.className = "currentInput";
+  answerScreen.className = "answerScreen";
+  answerScreen.style.color = "rgba(150, 150, 150, 0.87)";
+});
+
+function addToHistory(expression, result) {
+  history.push({ expression, result });
+  const historyList = document.getElementById("historyList");
+  const listItem = document.createElement("li");
+  listItem.textContent = `${expression} = ${result}`;
+  historyList.appendChild(listItem);
+
+  const historySection = document.getElementById("history");
+  if (history.length > 0) {
+    historySection.style.display = "block";
+  } else {
+    historySection.style.display = "none";
+  }
+}
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        if (!button.id.match('erase')) {
-            realTimeScreenValue.push(button.value)
-            currentInput.innerHTML = realTimeScreenValue.join('')
+  button.addEventListener("click", () => {
+    if (!button.id.match("erase")) {
+      realTimeScreenValue += button.value;
+      currentInput.innerHTML = realTimeScreenValue;
+    }
 
-            if (button.classList.contains('num_btn')) {
-                answerScreen.innerHTML = eval(realTimeScreenValue.join(''))
-            }
+    if (button.id.match("erase")) {
+      realTimeScreenValue = realTimeScreenValue.slice(0, -1);
+      currentInput.innerHTML = realTimeScreenValue;
+    }
 
-        }
+    if (button.id.match("evaluate")) {
+      currentInput.className = "answerScreen";
+      answerScreen.className = "currentInput";
+      answerScreen.style.color = "white";
 
-        if (btn.id.match('erase')) {
-            realTimeScreenValue.pop()
-            currentInput.innerHTML = realTimeScreenValue.join('')
-            answerScreen.innerHTML = eval(realTimeScreenValue.join(''))
-        }
-
-        if (btn.id.match('evaluate')) {
-            currentInput.className = 'answerScreen'
-            answerScreen.className = 'currentInput'
-            answerScreen.style.color = "white"
-        }
-
-        if (typeof eval(realTimeScreenValue.join('')) === 'undefined') {
-            answerScreen.innerHTML = 0
-        }
-
-    })
-})
+      if (realTimeScreenValue) {
+        const result = eval(realTimeScreenValue);
+        answerScreen.innerHTML = result;
+        addToHistory(realTimeScreenValue, result);
+      } else {
+        answerScreen.innerHTML = 0;
+      }
+    }
+  });
+});
